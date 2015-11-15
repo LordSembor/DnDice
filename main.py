31446acb223835bc33b19df37ca1cb2eac6519fd
Samuel Vogelsanger <vogelsangersamuel@hotmail.com>
2015-11-15 17:32:39 +0100

    cleaner main function

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ __author__ = 'piMoll, Lord_sembor'
 
 
 from d import d
-
+import numpy as np
 
 def main():
-	print((d(8)+7*d(6)+3)*3)
-
+	try:
+		print((d(8)+7*d(6)+3)*3)		# fails
+	except MemoryError:
+		print("'(d(8)+7*d(6)+3)*3' failed\n")
+	print(np.sort((3*d(6)).values))
 
 if __name__ == '__main__':
 	main()

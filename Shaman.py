93e8a89e3ce4e09ac5d058dc6fa94f23fdc50c16
LordSembor <bob_does_drugs@hotmail.com>
2015-11-16 15:51:45 +0100

    first draft of a crit-inclusive attack

--- Shaman.py
+++ Shaman.py
@@ -4,3 +4,17 @@ __author__ = 'sam <vogelsangersamuel@hotmail.com>'
 class Shaman(object):
 	def __init__(self, cleric, barbarian):
 		pass
+
+	def attack(self, attackRoll):
+		if isinstance(attackRoll, d):
+			result = d(0)
+			for i in attackRoll:
+				if attackRoll.values[i] > 18:
+					crit = (d(8)+7*d(6))+d(8)+10
+					result.stack(crit.values, crit.expectancies*attackRoll.expectancies[i])
+				elif attackRoll.values[i] > 13:
+					hit = d(8)+7*d(8)+10
+					result.stack(hit.values, hit.expectancies*attackRoll.expectancies[i])
+				else:
+					result.stack(np.r_[0], np.r_[attackRoll.expectancies[i]])
+			return result

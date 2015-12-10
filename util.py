6b38bd9fc529c5e832247593c33af4612fb700f3
Samuel Vogelsanger <vogelsangersamuel@hotmail.com>
2015-12-10 21:42:10 +0100

    some progress on [expectancy for n-th highest value at m dice thrown]

--- util.py
+++ util.py
@@ -2,14 +2,6 @@ from .d import d
 import numpy as np
 
 
-def advantage(dice=d(20)):
-	"""
-	@:param d dice
-	@:return d advantage
-	"""
-	return highest_of(2*dice)
-
-
 def gwf(dice=2*d(6)):
 	single_result = d()
 	count = len(dice.dice)
@@ -24,6 +16,14 @@ def gwf(dice=2*d(6)):
 	return result
 
 
+def advantage(dice=d(20)):
+	return highest_of(2*dice)
+
+
+def disadvantage(dice=d(20)):
+	return lowest_of(2*dice)
+
+
 def highest_of(dice):
 	v, e, l = __extrema_dice(dice)
 	return d(v, e, l)
@@ -33,14 +33,18 @@ def lowest_of(dice):
 	v, e, l = __extrema_dice(dice)
 	return d(v, e[::-1], l)
 
+
 def __extrema_dice(dice):
 	if not isinstance(dice, d):
 		raise ValueError
 	dice_count = len(dice.dice)
 	faces = dice.single().length
-	helper = np.arange(0, faces+1)**dice_count
-	counts = helper[1:] - helper[:-1]
-	expectancies = counts * (1/faces)**dice_count
-	values = np.arange(faces) + 1
+	expectancies = __roll_count(dice_count, faces)
+	values = np.arange(1, faces + 1)
 	return values, expectancies, faces
 
+
+def __roll_count(dice_count, faces):
+	counts = np.arange(0, faces + 1) ** dice_count
+	expectancies = d.normalize(counts[1:] - counts[:-1])
+	return expectancies
